e70dacf8c096b19082bd79cca1d8e6f5b9fba6e2
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-09-17 17:45:42 +0330

    registration login pages

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class AuthController extends Controller
+{
+    /**
+     * Write code on Method
+     *
+     * @return View ()
+     */
+    public function index(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return View ()
+     */
+    public function registration(): View
+    {
+        return view('auth.registration');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @param Request $request
+     * @return RedirectResponse ()
+     */
+    public function postLogin(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                ->withSuccess('You have Successfully loggedin');
+        }
+
+        return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @param Request $request
+     * @return RedirectResponse ()
+     */
+    public function postRegistration(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $data = $request->all();
+        $check = $this->create($data);
+
+        return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application ()
+     */
+    public function dashboard(): \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application
+    {
+        if (Auth::check()) {
+            return view('dashboard');
+        }
+
+        return redirect("login")->withSuccess('Opps! You do not have access');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return RedirectResponse ()
+     */
+    public function logout(): RedirectResponse
+    {
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
+Route::get('dashboard', [AuthController::class, 'dashboard']);
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
