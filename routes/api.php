9ad8c51a3a4c5847987e08a0d3a36bd26ef5a564
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-11-27 12:04:02 +0330

    api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\MemberController;
 use App\Models\City;
 use App\Models\Country;
 use App\Models\Language;
+use App\Models\Member;
 use App\Models\State;
 use App\Models\Upload;
 use Illuminate\Http\Request;
@@ -102,6 +103,49 @@ Route::get('/geoname/children', function (Request $request) {
     return State::all();
 });
 
+Route::post('/load-more/archive', function (Request $request) {
+     $members = Member::query()->limit(20)->get();
+
+     return '<div class="grid-item">
+                        <article class="model-card" itemscope
+                                 itemtype="https://schema.org/Person">
+                            <a href="./model-page?id={{ $member->id }}" itemprop="url">
+                                <meta itemprop="image"
+                                      content="/storage/assets/img/3x4/{{ $member->profile_image}}.webp">
+                                <img src="/storage/assets/img/lazy-3x4.webp" class="lazy"
+                                     data-src="/storage/assets/img/3x4/{{ $member->profile_image}}.webp"
+                                     width="200" height="260" alt="model">
+                                <noscript>
+                                    <img src="/storage/assets/img/3x4/{{ $member->profile_image}}.webp"
+                                         width="200" height="260"
+                                         alt="model">
+                                </noscript>
+                                @if($member->isNew)
+                                    <div class="card-metas">
+                                        <span class="meta-item">NEW FACE</span>
+                                    </div>
+                                @endif
+                                <div class="card-content">
+                                    <div class="card-inner-content">
+                                        <h2 class="card-primary-title" itemprop="name">{{ $member->name}}</h2>
+                                        <div class="card-content-item"
+                                             itemprop="nationality">{{ $member->nationality}}</div>
+                                        <div class="card-content-item">{{ $member->town}}</div>
+                                        <div class="card-content-item">{{ $member->height}} / {{ $member->bust}}
+                                            / {{ $member->waist}} / {{ $member->hips}} / {{ $member->age}}</div>
+                                        <div class="card-content-item">{{ $member->model_categories}}</div>
+                                    </div>
+                                    <div class="card-action">
+                                        <div class="btn btn-icon btn-add-model-to-bookmark">
+                                            <i class="icon-archive-add"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </article>
+                    </div>';
+
+});
 
 // /api/uploadphoto/
 Route::group([
