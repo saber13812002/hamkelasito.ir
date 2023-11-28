181a9945f47bdca80df4bbb943e46bc0669819db
Mohammad Shateri <32212145+MohammadShateri@users.noreply.github.com>
2023-11-28 09:50:31 +0330

    Revert "front - ui changes in html#10"
    This reverts commit 75a5e6b4d8b92cd3410c4c808af859f53b39e7e4.

--- resources/views/apply_as/footer.blade.php
+++ resources/views/apply_as/footer.blade.php
@@ -1,138 +1,45 @@
-<!-- start footer -->
-<footer id="footer">
-    <div class="container-xxl">
-        <div class="row">
 
-            <!-- start about us -->
-            <div class="col-12 col-sm-6 col-lg-4">
-                <div class="ft-about-us">
-                    <a href="/#" class="ft-site-logo">
-                        <img src="/storage/assets/img/lazy-trp-1x1.webp" class="lazy lz-dark"
-                             data-src="/storage/assets/img/logo-white.svg" width="88" height="24" alt="Liliana">
-                        <noscript>
-                            <img src="/storage/assets/img/logo-white.svg" width="88" height="24" alt="Liliana">
-                        </noscript>
+<!-- header -->
+<header id="header">
+    <div class="app-bar">
+        <div class="app-bar-container">
+            <div class="leading">
+                <button type="button" class="btn btn-icon" id="btn_ds_open_menu" aria-label="Open Menu">
+                    <i class="icon-menu"></i>
+                </button>
+                <button type="button" class="btn btn-icon" id="btn_ds_close_menu" aria-label="Close Menu">
+                    <i class="icon-close"></i>
+                </button>
+            </div>
+            <div class="app-bar-title">
+                <a href="/home" class="app-logo" aria-label="Liliana">
+                    <img src="/storage/assets/img/lazy-trp-1x1.webp" class="lazy"
+                         data-src="/storage/assets/img/logo.svg" width="88"
+                         height="24" alt="liliana">
+                    <noscript>
+                        <img src="/storage/assets/img/logo.svg" width="88" height="24" alt="liliana">
+                    </noscript>
+                </a>
+            </div>
+            <div class="actions">
+                <div class="app-lang">
+                    <a href="?locale=ja" class="btn btn-icon
+                     @if(App::getLocale() == 'ja')
+                     active
+                    @endif
+                    " aria-label="Change Language To Japane">
+                        JP
                     </a>
-                    <p>
-                        {{__('menu.We celebrate your uniqueness, help you achieve dreams, and support your creative journey at every step').'.'}}
-                    </p>
-                </div>
-                <div class="social-link">
-                    <a href="https://www.facebook.com/liliana.modeling/" class="btn btn-icon"
-                       aria-label="Facebook Link">
-                        <i class="icon-facebook"></i>
-                    </a>
-                    <a href="https://www.instagram.com/Liliana.models/" class="btn btn-icon"
-                       aria-label="Instagram Link">
-                        <i class="icon-instagram"></i>
-                    </a>
-                    <a href="https://www.tiktok.com/@lilianamodels" class="btn btn-icon" aria-label="Tiktok Link">
-                        <i class="icon-Tiktok"></i>
-                    </a>
-                    <a href="https://twitter.com/lilianamodels" class="btn btn-icon" aria-label="Twitter Link">
-                        <i class="icon-twitter"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/company/liliana-modeling" class="btn btn-icon"
-                       aria-label="LinkedIn Link">
-                        <i class="icon-linkedin"></i>
+                    <div class="divider"></div>
+                    <a href="?locale=en" class="btn btn-icon
+                     @if(App::getLocale() == 'en')
+                     active
+                    @endif
+                    " aria-label="Change language To English">
+                        EN
                     </a>
                 </div>
             </div>
-
-            <!-- start contact us -->
-            <div class="col-12 col-sm-6 col-lg-4">
-                <div class="ft-contact-us">
-                    <ul>
-                        <li>
-                            <i class="icon-call"></i>
-                            <a href="tel:+81-3-6362-3355">+81-3-6362-3355</a>
-                        </li>
-                        <li>
-                            <i class="icon-fax"></i>
-                            <a href="fax:+81-3-6323-0195">+81-3-6323-0195</a>
-                        </li>
-                        <li>
-                            <i class="icon-sms"></i>
-                            <a href="mailto:info@lilianamodels.com">info@lilianamodels.com</a>
-                        </li>
-                        <li>
-                            <i class="icon-location"></i>
-                            <span>
-                                {{__('menu.2nd floor, Vort Akasaka II, 4-4-8 Akasaka, Minato-ku, Tokyo 107-0052')}}
-                            </span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <!-- start menu and categories link -->
-            <div class="col-12 col-sm-12 col-lg-4 footer-more-links">
-                <div class="accordion">
-                    <div class="accordion-item">
-                        <button class="accordion-header">
-                            <span class="accordion-title">{{__('menu.Menu')}}</span>
-                            <i class="icon-arrow-down"></i>
-                        </button>
-                        <div class="accordion-content" style="display: none;">
-                            <div class="ft-menu">
-                                <ul>
-                                    <li><a href="/">{{__('menu.Home')}}</a></li>
-                                    <li><a href="work">{{__('menu.Portfolio')}}</a></li>
-                                    <li><a href="news-category">{{__('menu.News')}}</a></li>
-                                    <li><a href="about-us">{{__('menu.About us')}}</a></li>
-                                    <li><a href="contact-us">{{__('menu.Contact us')}}</a></li>
-                                    <li><a href="company-profile">{{__('menu.Company Profile')}}</a></li>
-                                    <li><a href="privacy-policy">{{__('menu.Privacy Policy')}}</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="accordion-item">
-                        <button class="accordion-header">
-                            <span class="accordion-title">{{__('menu.Categories')}}</span>
-                            <i class="icon-arrow-down"></i>
-                        </button>
-                        <div class="accordion-content" style="display: none;">
-                            <div class="ft-categories">
-                                <ul>
-                                    <li><a href="models-list">{{__('menu.Model')}}</a></li>
-                                    <li><a href="models-list">{{__('menu.Talent')}}</a></li>
-                                    <li><a href="models-list">{{__('menu.Actor')}}</a></li>
-                                    <li><a href="models-list">{{__('menu.Extra')}}</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- start social link mobile -->
-            <div class="col-12">
-                <div class="social-link">
-                    <a href="https://www.facebook.com/liliana.modeling/" class="btn btn-icon"
-                       aria-label="Facebook Link">
-                        <i class="icon-facebook"></i>
-                    </a>
-                    <a href="https://www.instagram.com/Liliana.models/" class="btn btn-icon"
-                       aria-label="Instagram Link">
-                        <i class="icon-instagram"></i>
-                    </a>
-                    <a href="https://www.tiktok.com/@lilianamodels" class="btn btn-icon" aria-label="Tiktok Link">
-                        <i class="icon-Tiktok"></i>
-                    </a>
-                    <a href="https://twitter.com/lilianamodels" class="btn btn-icon" aria-label="Twitter Link">
-                        <i class="icon-twitter"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/company/liliana-modeling" class="btn btn-icon"
-                       aria-label="LinkedIn Link">
-                        <i class="icon-linkedin"></i>
-                    </a>
-                </div>
-            </div>
-
         </div>
     </div>
-    <div class="copyright">
-        {{__('menu.©2023 Liliana Entertainment Agency, All Rights Reserved')}}.
-    </div>
-</footer>
+</header>
