d225ac205fe49c16d9cd1f5609d0084dd3691772
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-11-29 16:33:10 +0330

    import

---
+++ app/Console/Commands/dbimport.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class dbimport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:dbimport {}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
