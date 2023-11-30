26c9a3bcd42f5311fa0fd12a7ce0473ec4b72260
Mohammad Saleh Faezy <2158430-saleh.faezy77@users.noreply.gitlab.com>
2023-12-01 03:10:02 +0330

    some change

--- database/migrations/2023_11_30_111311_add_step_level_to_member_table.php
+++ database/migrations/2023_11_30_111311_add_step_level_to_member_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('members', function (Blueprint $table) {
-            $table->tinyInteger('level_step')->nullable();
+//            $table->tinyInteger('level_step')->nullable();
         });
     }
 
@@ -21,7 +21,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('members', function (Blueprint $table) {
-            $table->dropColumn('level_step');
+//            $table->dropColumn('level_step');
         });
     }
 };
