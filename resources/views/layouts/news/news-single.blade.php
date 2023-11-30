e6e6ac74880f4e3cc2c67970fedecbf4b07d6dd7
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-11-30 19:11:44 +0330

    bug buttons logout

--- resources/views/layouts/news/news-single.blade.php
+++ resources/views/layouts/news/news-single.blade.php
@@ -180,7 +180,7 @@
                                 <i class="icon-arrow-right"></i>
                             </span>
                             </a>
-                            <a href="#" class="logout">
+                            <a href="/logout" class="logout">
                             <span>
                                 Logout
                                 <i class="icon-logout"></i>
