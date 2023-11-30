dfb687db3fe4665c7485c011d904ff338b73ffd1
Saber tabatabaee yazdi <saber13812002@users.noreply.github.com>
2023-11-30 17:39:18 +0330

    ایجاد ساختار جدید صفحه ثبت نام (#40)
    Co-authored-by: TaHa <abootaha118@gmail.com>

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,122 @@
-@extends('layouts.app')
+@extends('layout-home')
+@section('content-home')
+    <body class="page page-account-form form-email form-email-model" style="visibility: visible;">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+    @include('header-site')
+    <!-- start main -->
+    <main>
+        <div id="content">
+            <div class="container-sm">
+                <form action="{{ route('register') }}" method="POST" class="account-form form-validation-on-submit">
+                    @csrf
+                    <div class="app-bar">
+                        <div class="app-bar-container">
+                            <div class="leading"></div>
+                            <div class="app-bar-title">
+                                <h1>{{ __('Register') }}</h1>
+                            </div>
+                            <div class="actions"></div>
+                        </div>
+                    </div>
+                    <div class="form-content">
+                        <i class="icon-account-email"></i>
+                        @if ($errors->has('email') || $errors->has('password') || $errors->has('name'))
+                            <div class="notifications-list active">
+                                <div class="notification-item danger">
+                                    <div class="notification-content">
+                                        <i class="icon-alert-triangle notification-icon"></i>
+                                        <div class="notification-inner-content">
+                                            <div class="notification-title">
+                                                @if ($errors->has('email'))
+                                                    {{ $errors->first('email') }}
+                                                @endif
+                                                @if ($errors->has('password'))
+                                                    {{ $errors->first('password') }}
+                                                @endif
+                                                @if ($errors->has('name'))
+                                                    {{ $errors->first('name') }}
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+                        <div class="form-description">
+                            Please Enter Your Email.
+                        </div>
+                        <div class="form-fields">
+                            <div>
+                                <div class="field field-type-nr-input" data-type="text" data-required="true"
+                                     data-required-type="name">
+                                    <div class="field-content">
+                                        <label for="email">{{ __('Name') }}</label>
+                                        <input type="text" placeholder="Entered Name" id="name"
+                                               class="form-control" name="name" value="{{ old('name') }}" required>
+                                        @error('name')
+                                            <div class="field-error-msg" id="field_error_name">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="field-error-msg" id="field_error_name"></div>
+                            </div>
+                            <div>
+                                <div class="field field-type-nr-input" data-type="text" data-required="true"
+                                     data-required-type="email">
+                                    <div class="field-content">
+                                        <label for="email">{{ __('Email Address') }}</label>
+                                        <input type="email" placeholder="Entered Email" id="email"
+                                               class="form-control" name="email" required>
+                                        @error('email')
+                                            <div class="field-error-msg" id="field_error_name">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="field-error-msg" id="field_error_email"></div>
+                            </div>
+                            <div>
+                                <div class="field field-type-nr-input" data-type="text" data-required="true">
+                                    <div class="field-content">
+                                        <label for="password">{{ __('Password') }}</label>
+                                        <input type="password" placeholder="Entered Password" id="password"
+                                               class="form-control" name="password" required>
+                                        @error('password')
+                                            <div class="field-error-msg" id="field_error_password">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <i class="icon-eye field-show-password"></i>
+                                </div>
+                                <div class="field-error-msg" id="field_error_password"></div>
+                            </div>
+                            <div>
+                                <div class="field field-type-nr-input" data-type="text" data-required="true">
+                                    <div class="field-content">
+                                        <label for="password">{{ __('Password') }}</label>
+                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('adminlte.register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="form-footer">
+                        <button type="submit"
+                                class="btn btn-primary btn-full ">{{ __('adminlte.register') }}</button>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
-</div>
+
+    </main>
+
+    <!-- start scripts -->
+    <script src="/storage/assets/js/env.js"></script>
+    <script src="/storage/assets/js/lib/jQuery.min.js"></script>
+    <script src="/storage/assets/js/lib/lazyload.min.js"></script>
+    <script src="/storage/assets/js/lib/owl.carousel.min.js"></script>
+    <script src="/storage/assets/js/super-slider.js"></script>
+    <script src="/storage/assets/js/pro-validation.min.js"></script>
+    <script src="/storage/assets/js/component.js"></script>
+    <script src="/storage/assets/js/scripts.js"></script>
+
+    </body>
 @endsection
