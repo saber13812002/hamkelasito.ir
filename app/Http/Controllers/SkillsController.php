6b40d6591a41e07eb4b016ae3991a69bdc52567f
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-12-02 14:57:34 +0330

    skills

---
+++ app/Http/Controllers/SkillsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Skills;
+use App\Http\Requests\StoreSkillsRequest;
+use App\Http\Requests\UpdateSkillsRequest;
+
+class SkillsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSkillsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Skills $skills)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Skills $skills)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSkillsRequest $request, Skills $skills)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Skills $skills)
+    {
+        //
+    }
+}
